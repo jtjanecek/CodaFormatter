370f75fadc3119b2afd1a33cb673927550606e61
John Janecek <janecektyler@gmail.com>
2020-10-24 16:21:16 -0700

    Refactor. Added CLI args, saves to output

--- CodaAuxFormater.py
+++
@@ -1,8 +0,0 @@
-import os
-from models.Chain import Chain
-
-chain_file = '/tmp/yassamri2/Tsukuba/hierarchical/storage/model_07/samples_1_chain1.txt'
-index_file = '/tmp/yassamri2/Tsukuba/hierarchical/storage/model_07/samples_1_index.txt'
-
-c = Chain(chain_file, index_file)
-c.save()
